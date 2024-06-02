72041e512a9c38b05f5d0e45c67cfa9790b20a6f
gprva <148754074+gprva@users.noreply.github.com>
2024-06-02 20:20:05 +0600

    delete unnecessary blocks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,29 +14,6 @@ export default function Home() {
   return (
     <>
       <div className="Home">
-        <div className="Home-container">
-          <h1>Welcome to Adornia</h1>
-          <h3>Elevate Every Moment with Adornia's Timeless Jewelry</h3>
-          <Button />
-
-        </div>
-        <div className="Home-wrap">
-          <div className="home_left">
-            <img src={right} alt="" />
-          </div>
-          <div className="home_right">
-            <h3 className="home-tit">About us</h3>
-            <p className="home-txt">
-              Established in 2008 in Korolev, our agency has been serving clients nationwide through our online platform. We pride ourselves on being a certified agency, accredited by both the Guild of Realtors of the Moscow Region and the Russian Guild of Realtors.
-
-              At our agency, your peace of mind is our priority. We understand the risks associated with real estate transactions, which is why we ensure that every deal is safeguarded. Did you know that a property transaction can be invalidated if the seller declares bankruptcy within a year prior to the sale? In such unfortunate cases, the court seizes the property to settle the seller's debts, leaving the buyer empty-handed. However, with us, such scenarios are mitigated – our comprehensive insurance coverage ensures that you are protected against any unforeseen circumstances.
-
-              When you choose our agency, you're choosing expertise and professionalism. Our team members undergo regular training and rigorous qualification exams to stay at the top of their game. Rest assured, you'll be working with knowledgeable professionals who have your best interests at heart.
-
-              Certified by industry standards, we guarantee top-notch service quality. Unlike uncertified agencies, we stand behind every transaction, assuming full responsibility for its success.
-            </p>
-          </div>
-        </div>
         <Slider slides={slides} />
       </div>
     </>
